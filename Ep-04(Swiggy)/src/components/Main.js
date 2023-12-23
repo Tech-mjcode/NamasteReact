f8fa12b7207531702040589f0c9594df47d3e85e
Manash <m@gmail.com>
2023-12-23 18:04:52 +0530

    useEffect

--- Ep-04(Swiggy)/src/components/Main.js
+++ Ep-04(Swiggy)/src/components/Main.js
@@ -1,11 +1,49 @@
 import dataList from "../utils/data";
 import ResCard from "./ResCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const MainComponent = () => {
   //hooks
-  //pass my dataList to ustState
-  const [data, setData] = useState(dataList);
+  //pass my dataList to useState
+  // const [data, setData] = useState(dataList);
+  const [data, setData] = useState([]);
+
+  //useEffect
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const res = await fetch("https://randomuser.me/api/");
+
+    // const data = await res.json();
+    // update data state variable
+    console.log(dataList);
+    setData(dataList);
+  };
+
+  //conditional rendering
+  if (data.length == 0) {
+    return (
+      <div className="shimmer-container">
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+      </div>
+    );
+  }
+
   return (
     <div className="main">
       <div className="filter">
